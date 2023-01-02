f56a00511b85286bc5f31011f16feeeceb1e7906
iDz <bdls1704@hotmail.com>
2023-01-01 23:10:03 -0300

    add vercel support

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const {
+  BACKEND_URL = 'http://localhost:3000',
+} = import.meta.env;
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BACKEND_URL,
   validateStatus() {
     return true;
   }
